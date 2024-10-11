8191d15075d3fd0abea6b9dbf89facfa53e9dacb
Smirnov Kirill <fmobail@bk.ru>
2024-10-11 21:06:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+/*function text(name , age) {
+    console.log("Hello My name is "+name+" , Age : "+age);
+    
+
+}
+ text("Kirill",13);
+ text("Kirill",12);
+ text("Kirill",15);
+ text("Kirill",17); */
+
+
+ /*let arr = [1,8,6,4,56,42,1]
+arr.sort(function(a,b){
+if (a==b) return 0
+if (a>b) return -1
+if (a<b) return 1
+})
+console.log(arr);
+*/
+/* console.log("Факториал числа N:")
+function factorial(n) {
+let fact = 1;
+for (let i=1 ; i<=n ; i++){ 
+    fact *=i ;
+}
+return fact ;
+}   
+let a = factorial(5)
+console.log(a);  */
+
+/*console.log("Вывести сумму цифр числа N:")
+let aandb = function(number) {
+ 
+let a = 0;
+for (let i = 0 ; i<number.length ; i++) {
+    a+=  +number[i] ;
+
+}  
+ return {"Сумма": a 
+
+ }
+} 
+console.log(aandb("625")); */
+
+/*console.log("Обратное число:");
+function rev(number){
+    number +="" ;
+    let revNumber ="" ;
+    for (let i=number.length-1; i>=0 ; i-- ) {
+        revNumber+= number[i] 
+
+
+    }
+    return +revNumber   
+}
+console.log(rev(625)) */
+
+/*console.log("Вывести четные и нечетные числа от 1 до 100:");
+function number(number){
+    
+    let chet = 0 ;
+    let nechet = 0 ;
+     
+    for (let i = 0 ; i<number ; i++){
+        if(i%2==0) chet ++  ;
+        if(i%2==1) nechet ++  ;
+    }
+    return{
+        "Четные числа": chet ,
+        "Нечетные числа": nechet,
+    }
+}
+console.log(number("100"))*/
+
+/*console.log("Вывести сумму чисел от 1 до N")
+function symma(num) {
+    let sum=0;
+    for(let i=0 ; i<=num ; i++){
+        sum+=i ;
+    }
+    return sum ;
+}
+console.log(symma(11)); */
+
